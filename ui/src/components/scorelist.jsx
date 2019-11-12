746298dfd98a5f20b33de7743e12aff7d020554e
Alistair Mersereau <alistairyves@gmail.com>
2019-11-11 23:02:32 -0500

    add mock db in package.json and update the scorelist

--- ui/src/components/scorelist.jsx
+++ ui/src/components/scorelist.jsx
@@ -3,14 +3,33 @@ import React, { Component } from "react";
 import Score from "./score";
 
 class ScoreList extends Component {
-  state = {
-    scores: ["Hello", "Goodbye", "see You later"]
-  };
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+  //   state = {
+  //     scores: ["Hello", "Goodbye", "see You later"]
+  //   };
+  componentDidMount() {
+    //todo get this part figured out
+    fetch("http://localhost:4000/users")
+      .then(results => {
+        return results.json;
+      })
+      .then(data => {
+        let users = data.results.map(user => {
+          return user;
+        });
+        this.setState({ users: users });
+      });
+  }
   render() {
     return (
       <div>
-        {this.state.scores.map(score => (
-          <Score value={score} />
+        {this.state.users.map(user => (
+          <Score value={user.value} />
         ))}
       </div>
     );
